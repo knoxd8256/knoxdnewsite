5946bc4420a6bf6acf8864633eeda06bca53f17b
David Knox <knoxd8256@connect.durhamtech.edu>
2019-11-03 19:53:22 -0500

    Removed portfolio page, reworked styling somewhat.

--- js/port.js
+++
@@ -1,4 +0,0 @@
-let collList = document.getElementsByTagName("button");
-for (let coll in collList) {
-    collList[coll].addEventListener("click", () => collList[coll].nextElementSibling.classList.toggle("show"));
-}
